08c079e29b6e10508255d1539ee1ed3351e280c9
dubeyanant <anantkumar312@gmail.com>
2023-12-24 14:37:14 +0530

    feat: add NA scale

--- rating.py
+++ rating.py
@@ -1,20 +1,24 @@
 def get_rating(parameter_name, rating_scale):
     while True:
         try:
-            print(f"Rate {parameter_name} for the book:")
-            for i, description in enumerate(rating_scale, start=1):
+            print(f"\nRate {parameter_name} for the book:")
+            for i, description in enumerate(rating_scale, start=0):
                 print(f"{i}: {description}")
 
-            rating = int(input("Enter your rating (1-5): "))
-            if 1 <= rating <= 5:
+            rating = int(input("Enter your rating (0-5): "))
+            if 0 <= rating <= 5:
                 return rating
             else:
-                print("Invalid input. Please enter a number between 1 and 5.")
+                print("Invalid input. Please enter a number between 0 and 5.")
         except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            print("Invalid input. Please enter a number between 0 and 5.")
 
 def calculate_average(ratings):
-    return sum(ratings) / len(ratings)
+    non_zero_ratings = [rating for rating in ratings if rating != 0]
+    if non_zero_ratings:
+        return round(sum(non_zero_ratings) / len(non_zero_ratings) * 2) / 2
+    else:
+        return 0  # If all ratings are 0, return 0
 
 def interpret_rating(average_rating):
     if average_rating < 2:
@@ -29,11 +33,11 @@ def interpret_rating(average_rating):
         return "Excellent"
 
 # Define rating scales
-initial_response_scale = ["Very negative", "Somewhat negative", "Indifferent", "Somewhat positive", "Very positive"]
-recommendation_scale = ["Would not recommend", "Might not recommend", "Neutral", "Would recommend", "Highly recommend"]
-re_readability_scale = ["Definitely not reread", "Unlikely to reread", "Neutral", "Likely to reread", "Definitely will reread"]
-plot_structure_scale = ["Not engaged at all", "Somewhat engaged", "Moderately engaged", "Very engaged", "Extremely engaged"]
-character_scale = ["Not relatable at all", "Somewhat relatable", "Moderately relatable", "Very relatable", "Extremely relatable"]
+initial_response_scale = ["N/A", "Very negative", "Somewhat negative", "Indifferent", "Somewhat positive", "Very positive"]
+recommendation_scale = ["N/A", "Highly discourage", "Discourage", "Neutral", "Encourage", "Highly encourage"]
+re_readability_scale = ["N/A", "Definitely not reread", "Unlikely to reread", "Neutral", "Likely to reread", "Definitely will reread"]
+plot_structure_scale = ["N/A", "Not engaged at all", "Somewhat engaged", "Moderately engaged", "Very engaged", "Extremely engaged"]
+character_scale = ["N/A", "Uninteresting", "Slightly relatable", "Moderately relatable", "Very relatable", "Incredibly relatable"]
 
 # Get ratings for each parameter
 initial_response = get_rating("Initial Response", initial_response_scale)
@@ -48,4 +52,4 @@ average_rating = calculate_average(ratings)
 
 # Interpret and display the result
 interpretation = interpret_rating(average_rating)
-print(f"\nAverage Rating: {average_rating:.2f} - {interpretation}")
+print(f"\nAverage Rating: {average_rating:.1f} - {interpretation}")
