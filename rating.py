456aab0c86c9d20d0515735caf305836dc58f6b8
dubeyanant <anantkumar312@gmail.com>
2023-12-24 14:18:56 +0530

    refactor: optimize code

--- rating.py
+++ rating.py
@@ -1,60 +1,10 @@
-def get_initial_response_rating():
-    print("Rate initial response for the book:")
-    print("1: Very negative | 2: Somewhat negative | 3: Indifferent | 4: Somewhat positive | 5: Very positive")
+def get_rating(parameter_name, rating_scale):
     while True:
         try:
-            rating = int(input("Enter your rating (1-5): "))
-            if 1 <= rating <= 5:
-                return rating
-            else:
-                print("Invalid input. Please enter a number between 1 and 5.")
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            print(f"Rate {parameter_name} for the book:")
+            for i, description in enumerate(rating_scale, start=1):
+                print(f"{i}: {description}")
 
-def get_recommendation_rating():
-    print("Rate recommendation for the book:")
-    print("1: Would not recommend | 2: Might not recommend | 3: Neutral | 4: Would recommend | 5: Highly recommend")
-    while True:
-        try:
-            rating = int(input("Enter your rating (1-5): "))
-            if 1 <= rating <= 5:
-                return rating
-            else:
-                print("Invalid input. Please enter a number between 1 and 5.")
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
-
-def get_re_readability_rating():
-    print("Rate Re-Readability for the book:")
-    print("1: Definitely not reread | 2: Unlikely to reread | 3: Neutral | 4: Likely to reread | 5: Definitely will reread")
-    while True:
-        try:
-            rating = int(input("Enter your rating (1-5): "))
-            if 1 <= rating <= 5:
-                return rating
-            else:
-                print("Invalid input. Please enter a number between 1 and 5.")
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
-
-def get_plot_rating():
-    print("Rate Plot of the book:")
-    print("1: Not engaged at all | 2: Somewhat engaged | 3: Moderately engaged | 4: Very engaged | 5: Extremely engaged")
-    while True:
-        try:
-            rating = int(input("Enter your rating (1-5): "))
-            if 1 <= rating <= 5:
-                return rating
-            else:
-                print("Invalid input. Please enter a number between 1 and 5.")
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
-
-def get_character_rating():
-    print("Rate Character of the book:")
-    print("1: Not relatable at all | 2: Somewhat relatable | 3: Moderately relatable | 4: Very relatable | 5: Extremely relatable")
-    while True:
-        try:
             rating = int(input("Enter your rating (1-5): "))
             if 1 <= rating <= 5:
                 return rating
@@ -78,12 +28,19 @@ def interpret_rating(average_rating):
     else:
         return "Excellent"
 
+# Define rating scales
+initial_response_scale = ["Very negative", "Somewhat negative", "Indifferent", "Somewhat positive", "Very positive"]
+recommendation_scale = ["Would not recommend", "Might not recommend", "Neutral", "Would recommend", "Highly recommend"]
+re_readability_scale = ["Definitely not reread", "Unlikely to reread", "Neutral", "Likely to reread", "Definitely will reread"]
+plot_structure_scale = ["Not engaged at all", "Somewhat engaged", "Moderately engaged", "Very engaged", "Extremely engaged"]
+character_scale = ["Not relatable at all", "Somewhat relatable", "Moderately relatable", "Very relatable", "Extremely relatable"]
+
 # Get ratings for each parameter
-initial_response = get_initial_response_rating()
-recommendation = get_recommendation_rating()
-re_readability = get_re_readability_rating()
-plot_structure = get_plot_rating()
-character = get_character_rating()
+initial_response = get_rating("Initial Response", initial_response_scale)
+recommendation = get_rating("Recommendation", recommendation_scale)
+re_readability = get_rating("Re-Readability", re_readability_scale)
+plot_structure = get_rating("Plot/Structure", plot_structure_scale)
+character = get_rating("Character", character_scale)
 
 # Calculate average rating
 ratings = [initial_response, recommendation, re_readability, plot_structure, character]
